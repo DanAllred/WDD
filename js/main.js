ad1490e962e6af01432b7c053ac8b581733229de
Dan Allred <82766686+DanAllred@users.noreply.github.com>
2022-01-22 11:23:58 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html",
-      label: "Week2 notes",
-      url: "week2/index.html",
-      label: "Week2 Team",
-      url: "week2/team.html"
-    }
-  ]
+function toc() {
+  const links = [{
+    label: "Week01 Notes",
+    url: "week1/index.html",
+    label: "Week02 notes",
+    url: "week2/index.html",
+    label: "Week02 Team",
+    url: "week2/team.html",
+    label: "Week02 notes",
+    url: "week2/index.html"
+  }]
+  document.getElementById("toc").innerHTML = links;
+}
